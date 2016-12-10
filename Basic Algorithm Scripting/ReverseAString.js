267a9b4595bc906676ad2899bb173ccff950a8ef
Ashish Patel <ashish.tripleclicks@gmail.com>
2016-12-10 18:09:06 +0530

    Create ReverseAString.js

---
+++ Basic Algorithm Scripting/ReverseAString.js
@@ -0,0 +1,8 @@
+/* Author: Ashish Patel | National Institute of Technology, Agartala  | MCA '16 | FCC 16-17
+
+function reverseString(str) {
+  var revStr = str.split('').reverse().join('');
+  return revStr;
+}
+
+reverseString("hello");
